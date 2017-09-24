3e434b13d2f788699e2c0af7e5843ae9198c4f23
Haseeb Mohammed <haseebmohammed@Haseebs-MacBook-Pro.local>
2017-09-24 15:20:17 -0500

    Added additional unit tests.

--- HelloWorldAPITests/Tests/HelloWorldAPIValuesControllerTest.cs
+++ HelloWorldAPITests/Tests/HelloWorldAPIValuesControllerTest.cs
@@ -1,6 +1,7 @@
 using System;
 using Xunit;
 using HelloWorldAPI;
+using HelloWorldAPI.Models;
 
 namespace HelloWorldAPITests
 {
@@ -10,13 +11,31 @@ namespace HelloWorldAPITests
         //TODO Add build command to copy over localization folder to keep files in sync with test project.
 
         [Fact]
-        public void ShouldReturnHelloWorld()
+        public void ShouldReturnHelloWorldInEnglish()
         {
             var controller = new HelloWorldAPI.Controllers.TranslateController();
 
-            var result = controller.Get("helloWorld");
+            TranslateModel wordToTranslate = new TranslateModel();
+            wordToTranslate.Culture = "en-US";
+            wordToTranslate.Word = "helloWorld";
+
+            var result = controller.Get(wordToTranslate);
 
             Assert.Equal("Hello World", result);
+        }
+
+        [Fact]
+        public void ShouldReturnHelloWorldInSpanish()
+        {
+            var controller = new HelloWorldAPI.Controllers.TranslateController();
+
+            TranslateModel wordToTranslate = new TranslateModel();
+            wordToTranslate.Culture = "es-ES";
+            wordToTranslate.Word = "helloWorld";
+
+            var result = controller.Get(wordToTranslate);
+
+            Assert.Equal("Hola Mundo", result);
         }
 
         [Fact]
@@ -24,9 +43,27 @@ namespace HelloWorldAPITests
         {
             var controller = new HelloWorldAPI.Controllers.TranslateController();
 
-            var result = controller.Get("SuperCallousFragileMysticHexedByHalitosis");
+            TranslateModel wordToTranslate = new TranslateModel();
+            wordToTranslate.Culture = "en-US";
+            wordToTranslate.Word = "SuperCallousFragileMysticHexedByHalitosis";
+
+            var result = controller.Get(wordToTranslate);
 
             Assert.Equal("-1", result);
+        }
+
+        [Fact]
+        public void ShouldReturnNull()
+        {
+            var controller = new HelloWorldAPI.Controllers.TranslateController();
+
+            TranslateModel wordToTranslate = new TranslateModel();
+            wordToTranslate.Culture = "zn-ZN";
+            wordToTranslate.Word = "helloWorld";
+
+            var result = controller.Get(wordToTranslate);
+
+            Assert.Equal(null, result);
         }
     }
 }
